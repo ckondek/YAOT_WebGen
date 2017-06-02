f3bbe795e225debafa461859a3af402e961cbfc5
ckondek <chef@loanshark.de>
2017-06-02 15:30:16 +0300

    add models

--- webGenApp/models.py
+++ webGenApp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Person(models.Model):
+    firstName = models.CharField(max_length=128, unique = True)
+    lastName = models.CharField(max_length=128, unique = True)
+    twitter = models.CharField(max_length=128, unique = True)
+    email_1 = models.EmailField(max_length=128, unique = True)
+    email_2 = models.EmailField(max_length=128, unique = True)
+    link_1 = models.URLField()
+    link_2 = models.URLField()
